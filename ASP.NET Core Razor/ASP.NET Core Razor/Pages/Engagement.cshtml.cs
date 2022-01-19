d831d71f2fec2bf2216ffe89e475b01c133ae52c
Houley <ethouley@gmail.com>
2022-01-19 16:45:28 +1000

    Setup Page for Engagements
    Added pages for CRUD.
    Currently displaying data.

---
+++ ASP.NET Core Razor/ASP.NET Core Razor/Pages/Engagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Core_Razor.Pages
+{
+    public class EngagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ASP.NET Core Razor/ASP.NET Core Razor/Pages/Engagement/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Core_Razor.Pages.Engagement
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
